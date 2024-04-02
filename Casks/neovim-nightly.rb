15265f8455425e21a9c98962485df5b46fcab2de
Daniel Kalintsev <d.kalintsev@gmail.com>
2024-04-02 20:34:57 +0200

    feat: adopt for intel and arm64

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -1,10 +1,18 @@
 cask 'neovim-nightly' do
   version :latest
   sha256 :no_check
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-x86_64.tar.gz"
   name 'Neovim Nightly'
   homepage 'https://neovim.io/'
-
-  binary 'nvim-macos-x86_64/bin/nvim'
+  
+  on_arm do
+    url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz"
+    binary 'nvim-macos-arm64/bin/nvim'
+  end
+  
+  on_intel do
+    url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-x86_64.tar.gz"
+    binary 'nvim-macos-x86_64/bin/nvim'
+  end  
 end
